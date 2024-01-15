37e592af86625cb12d59f05ae85e159d166abf97
bedovyy <bedovyy@gmail.com>
2024-01-16 04:47:36 +0900

    initial release

---
+++ modifiers.py
@@ -0,0 +1,25 @@
+# ref. https://huggingface.co/cagliostrolab/animagine-xl-3.0
+quality = [
+    "masterpiece",
+    "best quality",
+    "high quality",
+    "medium quality",
+    "normal quality",
+    "low quality",
+    "worst quality"
+]
+
+rating = [
+    "rating: general",
+    "rating: sensitive",
+    "rating: questionable",
+    "rating: explicit"
+]
+
+year = [
+    "newest",
+    "late",
+    "mid",
+    "early",
+    "oldest"
+]
